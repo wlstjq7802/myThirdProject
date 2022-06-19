1c8e900843c052a8a20405a417233a86983b621e
Jinseob Jang <wlstjq7802@naver.com>
2022-06-19 10:27:12 +0900

    second commit

--- ex3.php
+++ ex3.php
@@ -1,15 +1,6 @@
 <?php
 
-// require_once '/var/www/html/ex5.php'; 
-// require_once 'ex2.php';
-// //namespace test 
-// echo "en : ".language\en\welcome(); 
-// //Hello world! 
-// echo "<br>"; 
-// echo "kr : ".language\kr\welcome(); 
-// //안녕 세계! 
-// echo "<br>"; 
-//use test 
+
 use language\kr as kr;
 use language\en as en; 
 
